5167b29cbb9463c9d02c8cc29b409ae6c7e1768b
AidanB446 <138682950+AidanB446@users.noreply.github.com>
2024-04-19 13:37:56 -0400

    Add files via upload

---
+++ search.py
@@ -0,0 +1,20 @@
+
+import os
+
+def find_file(path, file) :
+    
+    return_dirs = []
+
+    for i in os.walk(path) :
+        if file in i[1] or file in i[2] :
+            return_dirs.append(i[0] + "/" + file)
+    
+
+    return return_dirs
+
+dir = input("search dir: ")
+file = input("target file or dir: ")
+
+for i in find_file(dir, file) :
+    print(i)
+
